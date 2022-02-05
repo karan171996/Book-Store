50457514bdc963a358ff516271d2edc766eda274
Karan <krnsngh38@gmail.com>
2022-02-05 19:27:42 +0530

    book layout fixed

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,22 +1,35 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./Shop.css";
+
+//book-image
+import book from "../asset/books.jpeg";
 
 const Shop = () => {
   const [product, setProduct] = useState([]);
 
   useEffect(() => {
-    const product = fetchProducts().then((res) => res.json());
-    console.log("product", product);
+    fetchProducts();
   }, []);
 
-  const fetchProducts = async () => {
-    const res = await axios.get("/api/shops/");
-    return await res;
+  const fetchProducts = () => {
+    axios.get("/api/shops").then((res) => {
+      setProduct(res.data.products);
+    });
   };
+  console.log("products", product);
   return (
-    <div>
-      <h1>My Products</h1>
-      <p>List of All the products....</p>
+    <div className="shop-container">
+      <div className="card-container">
+        {product.map((item, index) => (
+          <div className="card" key={index}>
+            <h3>{item?.title}</h3>
+            <img className="book-image" src={book} alt="book" />
+            <p>This is the random subtitle for this book</p>
+            <button>Add to Cart</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
