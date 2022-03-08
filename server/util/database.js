7a56c5b8811f2e3efc14b98674584b5b77471bd3
Karan <krnsngh38@gmail.com>
2022-03-08 23:44:26 +0530

    mongodb setuped

--- server/util/database.js
+++ server/util/database.js
@@ -1,10 +1,20 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
-const sequelize = new Sequelize(
-  "node-complete",
-  "root",
-  process.env.MYSQL_DB_PASSWORD,
-  { dialect: "mysql", host: "localhost" }
-);
-module.exports = sequelize;
+
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://karan171996:${encodeURIComponent(
+      process.env.MONGODB_PASSWORD
+    )}@cluster0.ilaku.mongodb.net/test?retryWrites=true&w=majority`
+  )
+    .then((client) => {
+      console.log("Connected");
+      callback(client);
+    })
+    .catch((err) => console.log("mongo error", err));
+};
+
+module.exports = mongoConnect;
